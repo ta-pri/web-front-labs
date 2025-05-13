f2e2225353120e2ca99c3f8eb39de8d4060a88bd
Privalova Taisiia <ta_privalova.ru@mail.ru>
2025-05-14 01:48:09 +0700

    Книги

---
+++ lab6/b.js
@@ -0,0 +1,13 @@
+const books = [
+    { author: 'Лао-цзы', name: 'Дао дэ цзин', publicationYear: 2022, price: 390, bestseller: true, available: true },
+    { author: 'Карл Саган', name: 'Космос', publicationYear: 2024, price: 550, bestseller: true, available: false },
+    { author: 'Баладжи Касал', name: 'Ошибки инвестора', publicationYear: 2025, price: 470, bestseller: false, available: true },
+    { author: 'Ф. Достоевский', name: 'Преступление и наказание', publicationYear: 2020, price: 310, bestseller: true, available: true },
+    { author: 'Джордж Оруэлл', name: '1984', publicationYear: 2021, price: 420, bestseller: true, available: true },
+    { author: 'Айн Рэнд', name: 'Атлант расправил плечи', publicationYear: 2023, price: 790, bestseller: false, available: false },
+    { author: 'Габриэль Гарсиа Маркес', name: 'Сто лет одиночества', publicationYear: 2019, price: 450, bestseller: true, available: true },
+    { author: 'Дэниел Канеман', name: 'Думай медленно... решай быстро', publicationYear: 2022, price: 560, bestseller: false, available: true },
+    { author: 'Юваль Харари', name: 'Sapiens', publicationYear: 2018, price: 620, bestseller: true, available: false },
+    { author: 'Марк Мэнсон', name: 'Тонкое искусство пофигизма', publicationYear: 2023, price: 430, bestseller: false, available: true }
+  ];
+  
---
+++ lab6/books.js
@@ -0,0 +1,18 @@
+const books = [
+    {
+      author: 'Лао-цзы',
+      name: 'Дао дэ цзин. Канон о Дао и дэ',
+      publicationYear: 2022
+    },
+    {
+      author: 'Карл Саган',
+      name: 'Космос',
+      publicationYear: 2024
+    },
+    {
+      author: 'Баладжи Касал',
+      name: 'Ошибки разумного инвестора',
+      publicationYear: 2025
+    },
+];
+  
