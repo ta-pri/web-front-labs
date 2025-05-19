0249d774047514b241e99c5a0f8978dfdc6b713c
Privalova Taisiia <ta_privalova.ru@mail.ru>
2025-05-20 00:09:11 +0700

    Страница о компании

--- lab6/books.js
+++ lab6/books.js
@@ -1,18 +1,83 @@
 const books = [
-    {
-      author: 'Лао-цзы',
-      name: 'Дао дэ цзин. Канон о Дао и дэ',
-      publicationYear: 2022
+    { 
+      author: 'Лев Толстой', 
+      name: 'Война и мир', 
+      publicationYear: 1869, 
+      price: 590, 
+      bestseller: true, 
+      available: true 
     },
-    {
-      author: 'Карл Саган',
-      name: 'Космос',
-      publicationYear: 2024
+    { 
+      author: 'Фёдор Достоевский', 
+      name: 'Преступление и наказание', 
+      publicationYear: 1866, 
+      price: 510, 
+      bestseller: false, 
+      available: true 
     },
-    {
-      author: 'Баладжи Касал',
-      name: 'Ошибки разумного инвестора',
-      publicationYear: 2025
+    { 
+      author: 'Александр Пушкин', 
+      name: 'Евгений Онегин', 
+      publicationYear: 1833, 
+      price: 420, 
+      bestseller: true, 
+      available: false 
     },
-];
+    { 
+      author: 'Михаил Булгаков', 
+      name: 'Мастер и Маргарита', 
+      publicationYear: 1967, 
+      price: 550, 
+      bestseller: true, 
+      available: true 
+    },
+    { 
+      author: 'Николай Гоголь', 
+      name: 'Мёртвые души', 
+      publicationYear: 1842, 
+      price: 470, 
+      bestseller: false, 
+      available: true 
+    },
+    { 
+      author: 'Иван Тургенев', 
+      name: 'Отцы и дети', 
+      publicationYear: 1862, 
+      price: 430, 
+      bestseller: false, 
+      available: false 
+    },
+    { 
+      author: 'Антон Чехов', 
+      name: 'Вишнёвый сад', 
+      publicationYear: 1904, 
+      price: 390, 
+      bestseller: false, 
+      available: true 
+    },
+    { 
+      author: 'Владимир Набоков', 
+      name: 'Лолита', 
+      publicationYear: 1955, 
+      price: 610, 
+      bestseller: true, 
+      available: true 
+    },
+    { 
+      author: 'Борис Пастернак', 
+      name: 'Доктор Живаго', 
+      publicationYear: 1957, 
+      price: 530, 
+      bestseller: false, 
+      available: true 
+    },
+    { 
+      author: 'Александр Солженицын', 
+      name: 'Архипелаг ГУЛАГ', 
+      publicationYear: 1973, 
+      price: 620, 
+      bestseller: true, 
+      available: false 
+    }
+  ];
   
